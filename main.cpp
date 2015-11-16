3c1696e19b7114215a26e31ad4e22b9755924a00
a-ewais <aa3_10000@yahoo.com>
2015-11-16 10:01:19 +0200

    the Json part

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include<vector>
 #include <string>
+#include<sstream>
+#include<map>
 #include "Circuit.h"
 #include "ParseError.h"
 
 using namespace std;
 using namespace vp;
+void JSONwave(map<string, string>, map<string, string>);
+void getSignalData(map<string, string>);
+string getData(string, bool, int);
+string dta[100];
+stringstream JSON;
 int xorr(vector<int> op)
 {
-	int x=0;
+	int x = 0;
 	for (int i = 0;i < op.size();i++)
 		x ^= op[i];
 	return x;
@@ -47,8 +54,8 @@ int buff(vector<int> op)
 {
 	return op[0];
 }
-namespace vp{
-	class mynode:protected Node {
+namespace vp {
+	class mynode :protected Node {
 	private:
 		bool ready;
 		int value;
@@ -56,7 +63,7 @@ namespace vp{
 		vector<int> op;
 		int new_tfall, new_trise;
 	public:
-		mynode(const Node& x):Node(x),ready(false),value(0),new_tfall(0),new_trise(0) {
+		mynode(const Node& x) :Node(x), ready(false), value(0), new_tfall(0), new_trise(0) {
 			if (this->isInputPort())
 				ready = true;
 			else if (type == "xor")
@@ -82,7 +89,7 @@ namespace vp{
 		void begin(new_circuit &x) {
 			for (int i = 0;i < this->getInputsCount();i++)
 			{
-				mynode &n = (x.node(x.getNodeIndex(inputNode(i).getName())));
+				mynode &n = (x.new_node(x.getNodeIndex(inputNode(i).getName())));
 				if (!n.is_ready())
 					n.begin(x);
 				op.push_back(n.get_value());
@@ -113,8 +120,8 @@ namespace vp {
 	class new_circuit :protected Circuit
 	{
 		vector<mynode> new_nodes;
-		
-	public:		
+
+	public:
 		new_circuit(Circuit& x) :Circuit(x) {
 			for (int i = 0;i < getNodesCount();i++)
 			{
@@ -125,6 +132,7 @@ namespace vp {
 		mynode& new_node(size_t index) {
 			return new_nodes[index];
 		}
+
 	};
 }
 
@@ -133,37 +141,15 @@ namespace vp {
 void printLn();
 
 int main() {
-	
+
 	try {
-		
+
 		// Initialize Circuit
 		Circuit circuit;
 		circuit.parseFile("FullAdder2.v");
 		circuit.parseDelaysFile("gateDelays.delay");
-		vector<mynode> new_circuit;
+		new_circuit operating(circuit);
 
-		for (int i = 0; i < circuit.getNodesCount(); i++) {	
-			/*cout << " Node " << i << endl;
-			cout << "  name:      " << circuit.node(i).getName() << endl;
-			cout << "  type:      " << circuit.node(i).getType() << endl;
-			cout << "  is input:  " << (circuit.node(i).isInputPort() ? "Yes" : "No") << endl;
-			cout << "  is output: " << (circuit.node(i).isOutputPort() ? "Yes" : "No") << endl;
-			cout << "  is gate:   " << (circuit.node(i).isGate() ? "Yes" : "No") << endl;
-			cout << "  trise:     " << circuit.node(i).getTRise() << endl;
-			cout << "  tfall:     " << circuit.node(i).getTFall() << endl;*/
-			/*cout << "  taba3y dah:" << endl;*/
-			mynode x(circuit.node(i));
-			new_circuit.push_back(x);
-			
-			if (new_circuit[i].isOutputPort())
-				output_nodes.push_back(new_circuit[i]);
-			else if (circuit.node(i).isInputPort())
-				input_nodes.push_back(new_circuit[i]);
-			printLn();
-		}
-
-		
-		
 	}
 	catch (ParseError& e) {
 		cout << "ParseError: " << e.what() << endl;
@@ -180,11 +166,111 @@ int main() {
 	return 0;
 }
 
-void printLn() {
-	int o = 78;
-	cout << " ";
-	while (o--) {
-		cout << "-";
+void JSONwave(map<string, string> input, map<string, string> output)
+{
+	string waveFinal;
+
+	JSON << "{\n\tconfig: {hscale:4, skin:'narrow'},\n\thead:{text:'Circuit 1', tick: 0,},\n";	//Initial Diagram configs
+	JSON << "\tsignal: [\n";	//Beggining of signal code
+
+
+								//Getting input Signals
+	getSignalData(input);
+
+	JSON << "\t\t{},\n";
+
+	//Getting output Signals
+	getSignalData(output);
+
+	JSON << "\t\t]\n}\n";			//END of signal code & wave
+
+	waveFinal = JSON.str();	//Copying info from stream.
+	cout << waveFinal;	//Outputting wavedrom code
+	system("pause");
+
+}
+
+//Reformats the data to wavedrom format
+string getData(string data, bool type, int count)
+{
+	char last = data[0];	//Initializing the comparator.
+	stringstream temp;
+
+
+	temp << "data: (";
+
+	//Looping and reformatting.
+	if (!type)
+		for (int i = 1; i < data.size(); i++)
+		{
+			if (data[i] == last) data[i] = '.';
+			else last = data[i];
+		}
+	else
+	{
+
+		for (int i = 0; i < dta[0].size(); i++)
+		{
+			string out;
+			for (int j = count; j >= 0; j--)
+				out += dta[j][i];
+
+			temp << "'" << out << "',";
+		}
+
+		temp << ")";
+		JSON << temp.str();
+
 	}
-	cout << endl;
+
+	return data;
+}
+
+void getSignalData(map<string, string> map1)
+{
+	for (auto it = map1.begin(); it != map1.end(); ++it)
+	{
+		if (it->first.find('[') > 0 && it->first.find('[') < it->first.size())
+		{
+			int size = it->first.find('[');	//Getting Variable name size
+			int Tcount = 0; //Total variables searches count.
+			string name = it->first.substr(0, size);	//Getting Variable name
+			dta[0] = it->second; //Initializing first value.
+			int count = 0; //Total variables of same bus count.
+
+			JSON << "\t\t{name: '" << name << "', wave: '";
+
+			for (int i = 0; i < it->second.size(); i++)
+				JSON << "=";
+
+			JSON << "', ";
+
+			for (int i = 1; i < map1.size() && it != map1.end(); i++)
+			{
+				it++;
+				int subset = it->first.find('[');
+				string sub = it->first.substr(0, subset);
+				Tcount++;
+
+				if (sub == name && sub.size() == size)
+				{
+					dta[i] = it->second;
+					count++;
+				}
+			}
+
+			while ((Tcount--) - count) it--;
+
+			getData(it->second, 1, count);
+			JSON << "},\n";
+
+		}
+		else
+		{
+			JSON << "\t\t{name: '" << it->first << "', wave: '" << getData(it->second, 0, 0) << "'}\n";
+		}
+
+
+	}
+
 }
